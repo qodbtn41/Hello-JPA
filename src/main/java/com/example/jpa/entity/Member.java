b348f68f509c409451c22efc38aa76f2cff6f84b
qodbtn41 <qodbtn41@naver.com>
2020-12-26 00:19:39 +0900

    ManyToOne의 cascade는 말이 안됨

--- src/main/java/com/example/jpa/entity/Member.java
+++ src/main/java/com/example/jpa/entity/Member.java
@@ -24,7 +24,7 @@ public class Member extends BaseEntity {
   private String city;
   private String street;
   private String zipcode;
-  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "TEAM_ID")
   private Team team;
 }
