c8db3cf7e1289c26a8b8a82434b552f201145b44
qodbtn41 <qodbtn41@naver.com>
2021-01-17 07:29:18 +0900

    도메인 클래스 컨버터

--- src/main/java/com/example/jpa/controller/MemberController.java
+++ src/main/java/com/example/jpa/controller/MemberController.java
@@ -12,24 +12,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
   private final MemberService memberService;
 
-  @GetMapping("/new")
+  @GetMapping("/members/new")
   public String createForm(Model model) {
     model.addAttribute("memberForm", new MemberForm());
     return "members/createMemberForm";
   }
 
-  @PostMapping("/new")
+  @PostMapping("/members/new")
   public String create(@Valid MemberForm form, BindingResult result) {
     if (result.hasErrors()) {
       return "members/createMemberForm";
@@ -44,10 +44,21 @@ public class MemberController {
     return "redirect:/";
   }
 
-  @GetMapping
+  @GetMapping("/members")
   public String list(Model model) {
     List<Member> members = memberService.findMembers();
     model.addAttribute("members", members);
     return "members/memberList";
+  }
+
+  @GetMapping("/members/v1/{id}")
+  public String findMember(@PathVariable("id") Long id) {
+    Member member = memberService.findMember(id);
+    return member.getName();
+  }
+
+  @GetMapping("/members/v2/{id}")
+  public String findMember(@PathVariable("id") Member member) {
+    return member.getName();
   }
 }
