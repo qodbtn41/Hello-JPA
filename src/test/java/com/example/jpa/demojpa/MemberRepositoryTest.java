25068579fd6322ebf8012e0cb4160007a17cc16e
qodbtn41 <qodbtn41@naver.com>
2021-01-17 03:26:46 +0900

    Update MemberRepositoryTest.java
    쿼리 메소드 기능
    추가 메서드 자동 생성. 메서드 이름을 통해서

--- src/test/java/com/example/jpa/demojpa/MemberRepositoryTest.java
+++ src/test/java/com/example/jpa/demojpa/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.example.jpa.demojpa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javax.transaction.Transactional;
 
 import com.example.jpa.domain.member.Member;
@@ -20,13 +22,20 @@ public class MemberRepositoryTest {
   @Test
   public void testMember() {
 
-    Member member = new Member();
-    member.setName("member A");
+    Member member = new Member("member A", 10);
     Member savedMember = memberRepository.save(member);
 
     Member findMember = memberRepository.findById(savedMember.getId()).get();
+
     Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
     Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
     Assertions.assertThat(findMember).isEqualTo(member);
+
+    Member findMember2 = memberRepository.findByName(savedMember.getName()).get(0);
+    Member findMember3 = memberRepository.findByNameAndAgeGreaterThan(savedMember.getName(), 5).get(0);
+
+    assertEquals("member A", findMember2.getName());
+    assertEquals(10, findMember3.getAge());
+
   }
 }
